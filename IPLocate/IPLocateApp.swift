aeb19ab0a80e553adc096c843c88112df8551132
cait <caitsy886@icloud.com>
2026-04-07 19:49:34 +0100

    Initial Commit

---
+++ IPLocate/IPLocateApp.swift
@@ -0,0 +1,21 @@
+//
+//  IPLocateApp.swift
+//  IPLocate
+//
+//  Created by Caitsy on 07/04/2026.
+//
+
+import SwiftUI
+import CoreData
+
+@main
+struct IPLocateApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
